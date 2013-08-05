fd9a5770e1f309ea31cd3996a8a165c0c6dc517e
Matthew Metnetsky <matthew@cowarthill.com>
2013-08-05 07:05:39 -0400

    test :: added test for encoding and decoding PID's

---
+++ test/encode.decode.pid.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+    , concat = require('concat-stream')
+    , encoder = require('../lib/encoder')
+    , decoder = require('../lib/decoder')
+    , EiPid = require('../lib/pid')
+    , ei = require('../lib/const')
+    , test = require('tap').test
+;
+
+test('encode/decode/pid', function(t) {
+    'use strict';
+
+    var enc, pid;
+
+    pid = new EiPid('nonost@nohost', 1, 2, 3);
+
+    enc = encoder();
+    enc.encodeVersion();
+    enc.encodePid(pid);
+
+    enc.pipe(concat(function(data) {
+        var dec = decoder(data);
+
+        t.ok(dec.decodeVersion(), 'version');
+        t.equals(dec.getType(), ei.PID_EXT, 'type');
+        t.deepEquals(dec.decodePid(), pid, 'value');
+        t.equals(dec.index, data.length, 'eof');
+        t.end();
+    }));
+});
