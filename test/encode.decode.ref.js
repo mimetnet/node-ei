e0d0d63f39be61dd266cf39360b011e66fdf9955
Matthew Metnetsky <matthew@cowarthill.com>
2013-08-06 23:57:08 -0400

    encoder.js :: implement encodeRef() and test

---
+++ test/encode.decode.ref.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+    , concat = require('concat-stream')
+    , encoder = require('../lib/encoder')
+    , decoder = require('../lib/decoder')
+    , EiRef = require('../lib/ref')
+    , ei = require('../lib/const')
+    , test = require('tap').test
+;
+
+test('encode/decode/ref', function(t) {
+    'use strict';
+
+    var enc, ref;
+
+    ref = new EiRef('nonode@nohost', 3, [94, 0, 0], 0);
+
+    enc = encoder();
+    enc.encodeVersion();
+    enc.encodeRef(ref);
+
+    enc.pipe(concat(function(data) {
+        var dec = decoder(data);
+
+        t.ok(dec.decodeVersion(), 'version');
+        t.equals(dec.getType(), ei.NEW_REFERENCE_EXT, 'type');
+        t.deepEquals(dec.decodeRef(), ref, 'value');
+        t.equals(dec.index, data.length, 'eof');
+        t.end();
+    }));
+});
